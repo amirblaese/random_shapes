b54bb8d0e3a0d1e4e14480a9b25b61e839458f1a
Shayan Gheidi <67720747+amirblaese@users.noreply.github.com>
2020-12-27 19:22:56 -0800

    commented the code

--- shapes.py
+++ shapes.py
@@ -9,14 +9,22 @@ import matplotlib.image as mpimg
 import shutil
 
 def paint(input,NUM_RECS,A_CONST,N_CONST,ASPECT_RANGE,MODE,DEBUG=False):
-  # NUM_RECS=iterations
-  # A_CONST=area_decay#0.35
-  # N_CONST=number_growth
-  # ASPECT_RANGE=aspect_ratio
-  # DEBUG=debug_mode
-  # MODE=model
-  img = mpimg.imread(input)
+  """
+  this function produces the art from the photo you input by drawing
+  rectangles that increase in frequency and reduce in area with each
+  iteration.
 
+  input: string, name of the file
+  NUM_RECS: int, number of iterations
+  A_CONST: float, decay constant for how rectangles' areas decrease
+  N_CONST: float, growth constant for how the # of rectangles grows
+  ASPECT_RANGE: int, the range of aspect ratios from 0..int and 0..1/int
+  MODE: 'e' or 'l' for exponential or linear mode. linear mode is prettier but takes longer
+  DEBUG: bool, turn on to see how your numbers for A_CONST and N_CONST change with iteration number
+  """
+
+  #read file
+  img = mpimg.imread(input)
 
 
 
@@ -26,40 +34,43 @@ def paint(input,NUM_RECS,A_CONST,N_CONST,ASPECT_RANGE,MODE,DEBUG=False):
 
 
 
-
+  #x and y resolution
   x=img.shape[0]
   y=img.shape[1]
 
+  #The actual RGB channel of the source image
   R=np.zeros((x,y),dtype=int)
   G=np.zeros((x,y),dtype=int)
   B=np.zeros((x,y),dtype=int)
   R[:,:]=img[:,:,0]
   G[:,:]=img[:,:,1]#100
   B[:,:]=img[:,:,2]#img[:,:,2]
-  new_img=np.zeros((x,y,3),dtype=int)
 
+  #empty array for new image
+  new_img=np.zeros((x,y,3),dtype=int)
+  #initialize it (not sure why I did this :))
   new_img[:,:,:]=0
   #new_img[:,:,1]=B
   #new_img[:,:,2]=G
 
 
-  N=[None]*NUM_RECS
-  AREAS=[None]*NUM_RECS
-  X_S=[None]*NUM_RECS
+  N=[None]*NUM_RECS #Number of rectangles per iteration
+  AREAS=[None]*NUM_RECS #Area of rectangles per iteration
+  X_S=[None]*NUM_RECS #starting/ending x and y position for each rectangle
   Y_S=[None]*NUM_RECS
-  X_E=[None]*NUM_RECS
+  X_E=[None]*NUM_RECS 
   Y_E=[None]*NUM_RECS
 
-  X_L=[None]*NUM_RECS
+  X_L=[None]*NUM_RECS #length of x and y of each rectangle
   Y_L=[None]*NUM_RECS
 
-  R_m=[None]*NUM_RECS
+  R_m=[None]*NUM_RECS  #empty lists for RGV means for each rectangle
   G_m=[None]*NUM_RECS
   B_m=[None]*NUM_RECS
 
-  ASPECTS=[None]*NUM_RECS
+  ASPECTS=[None]*NUM_RECS #aspect ratios for each iteration
 
-  xth=np.linspace(0,NUM_RECS,NUM_RECS+1)
+  xth=np.linspace(0,NUM_RECS,NUM_RECS+1) #iteration numbers for debug mode
   if MODE=='e':
     Nth=np.exp(N_CONST*xth)
     Ath=x*y*np.exp(-(Nth+1)*A_CONST)
@@ -67,14 +78,14 @@ def paint(input,NUM_RECS,A_CONST,N_CONST,ASPECT_RANGE,MODE,DEBUG=False):
     Nth=N_CONST*xth*10
     Ath=(-(xth)*A_CONST*1E5)+x*y
 
-  if DEBUG==True:
+  if DEBUG==True: #this just plots A_CONST and N_CONST as a function of iteration
     fig, (ax1, ax2) = plt.subplots(nrows=2)
     ax1.plot(xth,Nth,label='Number of rectangles')
     ax1.legend()
     ax2.plot(xth,Ath,label='Area of rectangles')
     ax2.legend()
     fig.tight_layout()
-
+  #this section is self explanatory really each iteration (i) has (j) boxes to plot and each is filled, etc
   if DEBUG==False:
     for i in range(NUM_RECS):
         #print('on',i,'th iteration')
@@ -126,7 +137,7 @@ def paint(input,NUM_RECS,A_CONST,N_CONST,ASPECT_RANGE,MODE,DEBUG=False):
           new_img[X_S[i][j]:X_E[i][j],Y_S[i][j]:Y_E[i][j],2]=B_m[i][j]
     plt.imshow(new_img)
     a=new_img
-    vv=a.astype('uint8')
+    vv=a.astype('uint8') #weird thing I had to do in order to save the figure
     plt.imsave(input[0:len(input)-4]+str(NUM_RECS)+str(int(A_CONST*100))+str(int(100*N_CONST))+str(ASPECT_RANGE)+str(MODE)+'.png',vv)
 
 
